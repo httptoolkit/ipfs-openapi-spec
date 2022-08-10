09b59f71a737376d62e34bd13f87352e49e2e457
Tim Perry <pimterry@gmail.com>
2022-08-10 16:39:54 +0200

    Add SPDX header

--- build-spec.ts
+++ build-spec.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Tim Perry <tim@httptoolkit.tech>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import _ from 'lodash';
 import * as fs from 'fs/promises';
 import { fetch } from 'undici';
