1d1d2c0faf35dc3bbb3979a04f8f9b21877ba531
Tim Perry <pimterry@gmail.com>
2022-08-10 10:41:40 +0200

    Fetch & parse the API's markdown
    Pinned to a single node version, since there's no rela reason to test
    compatibility, and node 14 doesn't include fetch in Undici, which is
    mildly inconvenient.

--- build-spec.ts
+++ build-spec.ts
@@ -1,1 +1,13 @@
-console.log('Hello world');
+import { fromMarkdown } from 'mdast-util-from-markdown';
+import { fetch } from 'undici';
+
+const API_MARKDOWN_URL = 'https://raw.githubusercontent.com/ipfs/ipfs-docs/main/docs/reference/kubo/rpc.md';
+
+const markdownResponse = await fetch(API_MARKDOWN_URL);
+if (!markdownResponse.ok) throw new Error(`Markdown response was ${markdownResponse.status}`);
+
+const markdownText = await markdownResponse.text();
+
+const markdownAST = fromMarkdown(markdownText);
+
+console.log(JSON.stringify(markdownAST, null, 2));
